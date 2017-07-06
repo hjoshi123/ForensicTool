705f2b504cefaa6fc7a2a9e82b1000f415a13bc4
Hemant Joshi <HemantJ@Hemant.local>
2017-07-06 20:24:31 +0530

    SMS INBOX

--- app/src/main/java/com/aditya/forensictool/MainActivity.java
+++ app/src/main/java/com/aditya/forensictool/MainActivity.java
@@ -1,13 +1,175 @@
 package com.aditya.forensictool;
 
+import android.Manifest;
+import android.content.ContentResolver;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Build;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int REQUEST_CODE_ASK_PERMISSIONS = 123;
+    private RecyclerView recyclerView;
+    private ArrayList<SMSData> sms = new ArrayList<>();
+    private UsersAdapter adapter = new UsersAdapter(this,sms);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        recyclerView = (RecyclerView) findViewById(R.id.recyclerview_main);
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.addItemDecoration(new DividerItemDecoration(this));
+
+        checkUserPermissions();
+        adapter.notifyDataSetChanged();
+
+    }
+
+    private void getSMSData(){
+        ContentResolver contentResolver = getContentResolver();
+        Uri uri = Uri.parse("content://sms/inbox");
+        Cursor cursor = contentResolver.query(uri,null,null,null,null);
+        String[] columns = new String[] { "address", "person", "date", "body","type" };
+        startManagingCursor(cursor);
+
+        // Read the sms data and store it in the list
+        if(cursor.moveToFirst()) {
+            for(int i=0; i < cursor.getCount(); i++) {
+                SMSData sms = new SMSData();
+                sms.setBody(cursor.getString(cursor.getColumnIndex("body")));
+                sms.setSenderNumber(cursor.getString(cursor.getColumnIndex("address")));
+                String date = cursor.getString(cursor.getColumnIndex(columns[2]));
+
+                Long timestamp = Long.parseLong(date);
+                Calendar calendar = Calendar.getInstance();
+                calendar.setTimeInMillis(timestamp);
+
+                Date finaldate = calendar.getTime();
+                String smsDate = finaldate.toString();
+                Log.d("MainAc", smsDate);
+                this.sms.add(sms);
+
+                cursor.moveToNext();
+            }
+        }
+        cursor.close();
+
+        adapter.notifyDataSetChanged();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        adapter.notifyDataSetChanged();
+    }
+
+    private class UsersAdapter extends RecyclerView.Adapter<UsersAdapter.ViewHolder> {
+
+        //SongInfo is the model name class
+        private ArrayList<SMSData> usersList;
+        private android.content.Context mContext;
+
+        public class ViewHolder extends RecyclerView.ViewHolder{
+
+            private TextView sender;
+            private TextView body;
+
+            public ViewHolder(View itemView) {
+                super(itemView);
+
+                sender = (TextView) itemView.findViewById(R.id.sender_name);
+                body = (TextView) itemView.findViewById(R.id.body);
+            }
+        }
+
+        public UsersAdapter(android.content.Context context, ArrayList<SMSData> usersList){
+            mContext = context;
+            UsersAdapter.this.usersList = usersList;
+        }
+
+        private android.content.Context getmContext(){
+            return mContext;
+        }
+
+        @Override
+        public UsersAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+
+            //R.layout.list_item is the layout xml dealing with how the elements of a single item in RecyclerView should appear
+            View view = getLayoutInflater().inflate(R.layout.list_item,parent,false);
+            ViewHolder viewHolder = new ViewHolder(view);
+
+            return viewHolder;
+        }
+
+        @Override
+        public void onBindViewHolder(UsersAdapter.ViewHolder holder, int position) {
+            SMSData details = UsersAdapter.this.usersList.get(position);
+
+            TextView name = holder.sender;
+            name.setText(details.getSenderNumber());
+
+            TextView body = holder.body;
+            body.setText(details.getBody());
+        }
+
+        @Override
+        public int getItemCount() {
+            return UsersAdapter.this.usersList.size();
+        }
+    }
+
+    private void checkUserPermissions(){
+        if ( Build.VERSION.SDK_INT >= 23){
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_SMS) !=
+                    PackageManager.PERMISSION_GRANTED  ){
+                requestPermissions(new String[]{
+                                Manifest.permission.READ_SMS},
+                        REQUEST_CODE_ASK_PERMISSIONS);
+                return ;
+            }
+        }
+
+        //if SDK is lesser than 23 then execute some function
+        getSMSData();
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_CODE_ASK_PERMISSIONS:
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // if permission is granted then execute the same function as above
+                    getSMSData();
+                } else {
+                    // Permission Denied
+                    Toast.makeText( this,"Media Permissions necessary" , Toast.LENGTH_SHORT)
+                            .show();
+                }
+                break;
+            default:
+                super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
     }
 }

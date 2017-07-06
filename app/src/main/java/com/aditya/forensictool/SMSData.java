10a42d1180bd3b65f49da801fdb8db0ad152eafb
Hemant Joshi <HemantJ@Hemant.local>
2017-07-06 20:34:34 +0530

    SMS INBOX and MODEL

---
+++ app/src/main/java/com/aditya/forensictool/SMSData.java
@@ -0,0 +1,26 @@
+package com.aditya.forensictool;
+
+/**
+ * Created by HemantJ on 06/07/17.
+ */
+
+public class SMSData {
+    private String senderNumber;
+    private String body;
+
+    public String getSenderNumber() {
+        return senderNumber;
+    }
+
+    public void setSenderNumber(String senderNumber) {
+        this.senderNumber = senderNumber;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+}

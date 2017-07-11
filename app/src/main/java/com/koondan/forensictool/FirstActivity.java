5feeb82007869e13d27aec17a4a945b6ae545cec
Hemant Joshi <HemantJ@Hemant.local>
2017-07-11 22:29:37 +0530

    Back to work

--- app/src/main/java/com/koondan/forensictool/FirstActivity.java
+++ app/src/main/java/com/koondan/forensictool/FirstActivity.java
@@ -5,78 +5,45 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Toast;
 
-import com.sergiocasero.revealfab.RevealFAB;
-
-
-public class FirstActivity extends AppCompatActivity {
+public class FirstActivity extends AppCompatActivity implements View.OnClickListener {
     private static final int REQUEST_CODE_ASK_PERMISSIONS = 123;
-    private RevealFAB smsButton, callLogsButton;
-    private Intent smsIntent;
-    private Intent phoneIntent;
-    View firstView;
+    private FloatingActionButton smsButton, callLogsButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_first);
 
-        smsIntent = new Intent(FirstActivity.this, MainActivity.class);
+        smsButton = (FloatingActionButton) findViewById(R.id.sms_button);
+        callLogsButton = (FloatingActionButton) findViewById(R.id.call_logs_button);
 
-        phoneIntent = new Intent(FirstActivity.this, MainActivity.class); //Change class here after completing phone
-
-
-        smsButton = (RevealFAB) findViewById(R.id.sms_button);
-        callLogsButton = (RevealFAB) findViewById(R.id.call_logs_button);
-
-        smsButton.setIntent(smsIntent);
-        callLogsButton.setIntent(phoneIntent);
-
-        smsButton.setOnClickListener(new RevealFAB.OnClickListener() {
-            @Override
-            public void onClick(RevealFAB button, View v) {
-                checkUserPermissions(button);
-            }
-        });
-
-        callLogsButton.setOnClickListener(new RevealFAB.OnClickListener() {
-            @Override
-            public void onClick(RevealFAB button, View v) {
-                checkUserPermissions(button);
-            }
-        });
+        smsButton.setOnClickListener(this);
+        callLogsButton.setOnClickListener(this);
 
     }
-
 
     @Override
-    protected void onResume() {
-        super.onResume();
-        smsButton.onResume();
-        callLogsButton.onResume();
+    public void onClick(View v) {
+        switch(v.getId()){
+            case R.id.sms_button:
+                checkUserPermissions();
+                break;
+            case R.id.call_logs_button:
+                Toast.makeText(this,"Milestone 3",Toast.LENGTH_SHORT).show();
+                break;
+            default:
+                Toast.makeText(this,"Nothing Here",Toast.LENGTH_SHORT).show();
+        }
     }
 
-
-
-    //    @Override
-//    public void onClick(View v) {
-//        switch(v.getId()){
-//            case R.id.sms_button:
-//                checkUserPermissions(smsButton);
-//                break;
-//            case R.id.call_logs_button:
-//                Toast.makeText(this,"Milestone 3",Toast.LENGTH_SHORT).show();
-//                break;
-//            default:
-//                Toast.makeText(this,"Nothing Here",Toast.LENGTH_SHORT).show();
-//        }
-//    }
-
-    private void checkUserPermissions(RevealFAB revealFAB){
+    private void checkUserPermissions(){
         if ( Build.VERSION.SDK_INT >= 23){
             if (ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_SMS) !=
                     PackageManager.PERMISSION_GRANTED  ){
@@ -88,8 +55,7 @@ public class FirstActivity extends AppCompatActivity {
         }
 
         //if SDK is lesser than 23 then execute some function
-
-        revealFAB.startActivityWithAnimation();
+        startActivity(new Intent(FirstActivity.this,MainActivity.class));
 
     }
 
@@ -110,5 +76,4 @@ public class FirstActivity extends AppCompatActivity {
                 super.onRequestPermissionsResult(requestCode, permissions, grantResults);
         }
     }
-
 }

c307bf32138d36d550f74f8efb424a14e58d5f38
Aditya Gupta <adi.gupta13@gmail.com>
2017-07-12 11:40:16 +0530

    Changed DB names

--- app/src/main/java/com/koondan/forensictool/FirstActivity.java
+++ app/src/main/java/com/koondan/forensictool/FirstActivity.java
@@ -1,6 +1,7 @@
 package com.koondan.forensictool;
 
 import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Build;
@@ -14,7 +15,8 @@ import android.widget.Toast;
 public class FirstActivity extends AppCompatActivity implements View.OnClickListener {
     private static final int REQUEST_CODE_ASK_PERMISSIONS = 123;
     private FloatingActionButton smsButton, callLogsButton;
-
+    int PERMISSION_ALL = 1;
+    String[] PERMISSIONS = {Manifest.permission.READ_SMS, Manifest.permission.WRITE_EXTERNAL_STORAGE};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,11 +47,9 @@ public class FirstActivity extends AppCompatActivity implements View.OnClickList
 
     private void checkUserPermissions(){
         if ( Build.VERSION.SDK_INT >= 23){
-            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_SMS) !=
-                    PackageManager.PERMISSION_GRANTED  ){
-                requestPermissions(new String[]{
-                                Manifest.permission.READ_SMS},
-                        REQUEST_CODE_ASK_PERMISSIONS);
+            if(!hasPermissions(this, PERMISSIONS)){
+                ActivityCompat.requestPermissions(this, PERMISSIONS, PERMISSION_ALL);
+
                 return ;
             }
         }
@@ -59,21 +59,33 @@ public class FirstActivity extends AppCompatActivity implements View.OnClickList
 
     }
 
-    @Override
-    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
-        switch (requestCode) {
-            case REQUEST_CODE_ASK_PERMISSIONS:
-                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    // if permission is granted then execute the same function as above
-                    startActivity(new Intent(FirstActivity.this,MainActivity.class));
-                } else {
-                    // Permission Denied
-                    Toast.makeText(this,"Media Permissions necessary" , Toast.LENGTH_SHORT)
-                            .show();
+//    @Override
+//    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+//        switch (requestCode) {
+//            case REQUEST_CODE_ASK_PERMISSIONS:
+//                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+//                    // if permission is granted then execute the same function as above
+//                    startActivity(new Intent(FirstActivity.this,MainActivity.class));
+//                } else {
+//                    // Permission Denied
+//                    Toast.makeText(this,"Media Permissions necessary" , Toast.LENGTH_SHORT)
+//                            .show();
+//                }
+//                break;
+//            default:
+//                super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+//        }
+//    }
+
+
+    public static boolean hasPermissions(Context context, String... permissions) {
+        if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && context != null && permissions != null) {
+            for (String permission : permissions) {
+                if (ActivityCompat.checkSelfPermission(context, permission) != PackageManager.PERMISSION_GRANTED) {
+                    return false;
                 }
-                break;
-            default:
-                super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+            }
         }
+        return true;
     }
 }

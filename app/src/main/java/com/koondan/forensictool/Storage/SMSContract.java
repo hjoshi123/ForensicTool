998919139c80dc3c986e172bc5eb6e1d52759f81
Aditya Gupta <adi.gupta13@gmail.com>
2017-07-12 17:46:50 +0530

    Changed Address to Phone Number in DB

--- app/src/main/java/com/koondan/forensictool/Storage/SMSContract.java
+++ app/src/main/java/com/koondan/forensictool/Storage/SMSContract.java
@@ -11,7 +11,7 @@ public final class SMSContract {
     public static final class SMSEntry implements BaseColumns {
         public static final String TABLE_NAME = "sms";
 
-        public static final String COLUMN_SENDER_ADDRESS = "address";
+        public static final String COLUMN_SENDER_ADDRESS = "phone_number";
         public static final String COLUMN_MSG_BODY = "body";
         public static final String COLUMN_MSG_DATE = "date";
         public static final String COLUMN_MSG_THREAD = "thread_id";
